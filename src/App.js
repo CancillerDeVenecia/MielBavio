ae6aeb0e11c7d29f6506b37cb853351bcff0020a
Canciller <manuelgarzon1236@gmail.com>
2021-09-29 23:02:49 -0300

    adding a Cart item and a greeting interface

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+
+// Components
 import NavBar from './components/navBar/NavBar';
+import ItemListContainer from './components/itemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting="Bienvenidos a Miel Bavio!"/>
     </div>
   );
 }
---
+++ src/components/cartWidget/CartWidget.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartWidget = () => {
+    return (
+        <i class="yellow large shopping cart icon"></i>
+    )
+}
+
+export default CartWidget

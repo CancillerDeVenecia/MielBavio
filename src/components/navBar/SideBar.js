fa1447c1c1dc332a56a2ffeea15c0da56451b8f6
Canciller <manuelgarzon1236@gmail.com>
2021-11-01 21:04:14 -0300

    sume renderizado condicional al cart widget

--- src/components/navBar/SideBar.js
+++ src/components/navBar/SideBar.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import CartWidget from "../cartWidget/CartWidget";
 import { Link } from 'react-router-dom';
+import { CartContext } from '../../context/CartContext';
 
 let w3_close = () => {
     document.getElementById("mySidebar").style.display = "none";
@@ -9,6 +10,9 @@ let w3_close = () => {
 
 
 const SideBar = () => {
+
+    const { cartItems } = useContext(CartContext);
+
     return (
         <div>
             <div className="w3-sidebar w3-bar-block w3-border-right" style={{display: "none"}} id="mySidebar">
@@ -16,9 +20,12 @@ const SideBar = () => {
                 <Link to="/" className="w3-bar-item w3-button">Productos</Link>
                 <Link to="/contact" className="w3-bar-item w3-button">Contacto</Link>
                 <Link to="/about" className="w3-bar-item w3-button">Acerca de Miel Bavio</Link>
-                <Link to="/Cart" className="w3-bar-item w3-button">
-                    <CartWidget/>
-                </Link>
+                {cartItems.length > 0 &&
+                    <Link to="/Cart" className="w3-bar-item w3-button">
+                        <CartWidget/> <p style={{display: "inline"}}>{cartItems.length}</p>
+                    </Link>
+                }
+                
             </div>
         </div>
     )

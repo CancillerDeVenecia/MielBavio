1b1b8e7573e6aa89b310dd64afa7281f0448c6ad
Canciller <manuelgarzon1236@gmail.com>
2021-11-24 00:30:01 -0300

    ultima subida

--- src/views/Buy/Buy.js
+++ src/views/Buy/Buy.js
@@ -2,6 +2,7 @@ import { collection, addDoc } from "firebase/firestore";
 import { db } from '../../Firebase';
 import React, {useState, useContext} from 'react';
 import { CartContext } from '../../context/CartContext';
+import { Dimmer, Loader } from 'semantic-ui-react';
 
 
 
@@ -12,6 +13,10 @@ const Buy = () => {
     const [final, setFinal] = useState(0)
     const [idTransaccion, setIdTransaccion] = useState("")
     const [compraRealizada, setCompraRealizada] = useState(false);
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [direction, setDirection] = useState("")
+    const [onLoad, setOnLoad] = useState(false)
 
     useState(() => {
     let total = 0;
@@ -26,13 +31,34 @@ const Buy = () => {
 
 
 const sendBuy = async () => {
+    setOnLoad(true);
+    const date = new Date();
 
     const docRef = await addDoc(collection(db, "pedidos"), {
         total: final,
         detail: cartItems,
+        buyer: {
+                name: name,
+                surname: surname,
+                direction: direction
+            },
+        date: date.toString()
     });
     setIdTransaccion(docRef.id);
     setCompraRealizada(true);
+    setOnLoad(false);
+}
+
+const onNameChange = (e) => {
+    setName(e.target.value);
+}
+
+const onSurnameChange = (e) => {
+    setSurname(e.target.value);
+}
+
+const onDirectionChange = (e) => {
+    setDirection(e.target.value);
 }
 
     return (
@@ -51,19 +77,20 @@ const sendBuy = async () => {
                     
                     <div>
                         <label>Nombre</label>
-                        <input className="w3-input" type="text"></input>
+                        <input className="w3-input" type="text" onChange={onNameChange} value={name}></input>
 
                         <label>Apellido</label>
-                        <input className="w3-input" type="text"></input>
+                        <input className="w3-input" type="text" onChange={onSurnameChange} value={surname}></input>
 
                         <label>Dirección</label>
-                        <input className="w3-input" type="text"></input>
+                        <input className="w3-input" type="text" onChange={onDirectionChange} value={direction}></input>
                     </div>
 
                     <button className= "w3-button w3-theme w3-large w3-margin" onClick={() => sendBuy()}>Terminar compra</button>
 
                 </div>
             }
+            <Dimmer active={onLoad}><Loader/></Dimmer>
         </div>
     )
 }
